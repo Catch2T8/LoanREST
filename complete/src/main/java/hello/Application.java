e863be245d34644cf8473a2b120494e12169cdff
Michael Lance Domagas <mdomagas@hotmail.com>
2016-12-02 20:14:46 +0800

    Added hello world

--- complete/src/main/java/hello/Application.java
+++ complete/src/main/java/hello/Application.java
@@ -14,6 +14,7 @@ public class Application extends SpringBootServletInitializer {
     }
 
     public static void main(String[] args) {
+        System.out.println("Hello world");
         SpringApplication.run(Application.class, args);
     }
 }
